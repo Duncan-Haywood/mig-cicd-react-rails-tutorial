f67c5ae4cf335432e427b91c6ed87f87ff06b67f
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-06 15:32:19 -0700

    updates script for user input in personalizing gcp compute create subnet

--- set-up-gcloud.sh
+++ set-up-gcloud.sh
@@ -1,21 +1,24 @@
+#!/bin/bash
 gcloud auth login;
 gcloud init;
 
-print Creating a custom VPC network
+echo Creating a custom VPC network
 gcloud compute networks create my-lb-network --subnet-mode=custom;
 
-print Creating subnet
-gcloud compute networks subnets create my-subnet \
+echo Creating subnet
+read -p "Please enter a name for your subnet; example: my-subnet "  my-subnet
+echo "Creating subnet with name $my-subnet"
+gcloud compute networks subnets create $my-subnet \
   --network=my-lb-network \
   --range=10.1.10.0/24 \
   --region=southamerica-east1;
 
-print Reserving the IP addresses
+echo Reserving the IP addresses
 gcloud compute addresses create my-lb-ipv4 \
   --ip-version=IPV4 \
   --global;
 
-print Creating firewall rule to allow healthchecks and incoming traffic
+echo Creating firewall rule to allow healthchecks and incoming traffic
 gcloud compute firewall-rules create my-fw-allow-health-and-proxy \
   --network=my-lb-network \
   --action=allow \
@@ -24,7 +27,7 @@ gcloud compute firewall-rules create my-fw-allow-health-and-proxy \
   --source-ranges=130.211.0.0/22,35.191.0.0/16 \
   --rules=tcp:80,tcp:443,tcp:3000;
 
-print Creating firewall rule to allow SSH 
+echo Creating firewall rule to allow SSH 
 gcloud compute firewall-rules create my-fw-allow-ssh \
   --network=my-lb-network \
   --action=allow \
@@ -32,7 +35,7 @@ gcloud compute firewall-rules create my-fw-allow-ssh \
   --target-tags=allow-ssh \
   --rules=tcp:22;
 
-print Creating instance template
+echo Creating instance template
 gcloud compute instance-templates create-with-container my-first-template \
   --custom-cpu=1 \
   --custom-memory=2GB \
@@ -43,32 +46,32 @@ gcloud compute instance-templates create-with-container my-first-template \
   --tags=allow-hc-and-proxy,allow-ssh \
   --container-image gcr.io/my-project-id/my-image-name;
 
-print Creating the managed instance group
+echo Creating the managed instance group
 gcloud compute instance-groups managed create my-mig \
   --base-instance-name my-instance \
   --size 3 \
   --template my-first-template \
   --region southamerica-east1;
 
-print Creating port forwarding
+echo Creating port forwarding
 gcloud compute instance-groups managed set-named-ports my-mig \
   --named-ports port3000:3000 \
   --region southamerica-east1;
 
-print Creating health check
+echo Creating health check
 gcloud compute health-checks create http my-http-check \
   --port 3000 \
   --request-path=/health_check \
   --healthy-threshold=1 \
   --unhealthy-threshold=10
 
-print Assigning health check to the managed instance group
+echo Assigning health check to the managed instance group
 gcloud compute instance-groups managed update my-mig \
   --health-check my-http-check \
   --initial-delay 300 \
   --region southamerica-east1
 
-print Creating a backend service
+echo Creating a backend service
 gcloud compute backend-services create my-backend-service \
   --protocol HTTP \
   --health-checks my-http-check \
@@ -76,7 +79,7 @@ gcloud compute backend-services create my-backend-service \
   --port-name=port3000 \
   --enable-cdn
 
-print Assigning instance group to the backend service
+echo Assigning instance group to the backend service
 gcloud compute backend-services add-backend my-backend-service \
   --balancing-mode=UTILIZATION \
   --max-utilization=0.8 \
@@ -85,66 +88,66 @@ gcloud compute backend-services add-backend my-backend-service \
   --instance-group-region=southamerica-east1 \
   --global
 
-print Creating storage bucket
+echo Creating storage bucket
 gsutil mb -c standard -l southamerica-east1 -b on gs://my-bucket.example.com.br
 
-print Making the bucket public
+echo Making the bucket public
 gsutil iam ch allUsers:objectViewer gs://my-bucket.example.com.br
 
-print Configuring bucket for web
+echo Configuring bucket for web
 gsutil web set -m index.html -e index.html gs://my-bucket.example.com.br
 
-print Creating backend-bucket using our newly created buckets
+echo Creating backend-bucket using our newly created buckets
 gcloud compute backend-buckets create my-backend-bucket \
   --gcs-bucket-name=gs://my-bucket.example.com.br \
   --enable-cdn
 
-print Creating a URL map
+echo Creating a URL map
 gcloud compute url-maps create my-lb-map \
   --default-backend-bucket my-backend-bucket
 
-print Adding path matcher to the URL map (backend)
+echo 'Adding path matcher to the URL map (backend)'
 gcloud compute url-maps add-path-matcher my-lb-map \
   --default-service my-backend-service \
   --path-matcher-name my-pathmap-backend \
   --new-hosts=my-backend.example.com.br
 
-print Adding path matcher to the URL map (frontend)
+echo Adding path matcher to the URL map (frontend)
 gcloud compute url-maps add-path-matcher my-lb-map \
   --default-backend-bucket my-backend-bucket \
   --path-matcher-name my-pathmap-frontend \
   --new-hosts=my-bucket.example.com.br
 
-print Creating a managed certificate 
+echo Creating a managed certificate 
 gcloud beta compute ssl-certificates create my-mcrt \
   --domains my-bucket.example.com.br,my-backend.example.com.br
 
-print Creating an https proxy
+echo Creating an https proxy
 gcloud compute target-https-proxies create my-https-proxy \
   --url-map my-lb-map \
   --ssl-certificates my-mcrt
 
-print Creating forwarding rules to proxy
+echo Creating forwarding rules to proxy
 gcloud compute forwarding-rules create my-forwarding-rule \
   --address=my-lb-ipv4 \
   --global \
   --target-https-proxy=my-https-proxy \
   --ports=443
 
-print Creating address range for db private network
+echo Creating address range for db private network
 gcloud compute addresses create my-sql-network-ranges \
   --global \
   --purpose=VPC_PEERING \
   --prefix-length=24 \
   --network=my-lb-network
 
-print Creating the VPC peering
+echo Creating the VPC peering
 gcloud services vpc-peerings connect \
   --service=servicenetworking.googleapis.com \
   --ranges=my-sql-network-ranges \
   --network=my-lb-network
 
-print Creating db instance
+echo Creating db instance
 gcloud beta sql instances create my-database-2 \
   --network=my-lb-network \
   --tier=db-n1-standard-1 \
